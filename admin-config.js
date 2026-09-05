082febc2bba869a3557b809834e506fe745f25ce
Prantik Das <dasprantik76@gmail.com>
2026-09-05 15:09:58 +0530

    Configure separated admin portal

--- admin-config.js
+++ admin-config.js
@@ -3,12 +3,12 @@
  * Add each independently deployed public academy URL to publicSites.
  */
 window.ADMIN_PORTAL_CONFIG = Object.freeze({
-  defaultPublicSiteUrl: 'https://YOUR-PUBLIC-SITE.vercel.app',
+  defaultPublicSiteUrl: 'https://digantacomputer.vercel.app',
   authorizedAdminEmails: Object.freeze([
     'poulami.13thmay@gmail.com',
     'dasprantik76@gmail.com'
   ]),
   publicSites: Object.freeze({
-    prantik: 'https://YOUR-PUBLIC-SITE.vercel.app'
+    prantik: 'https://digantacomputer.vercel.app'
   })
 });
